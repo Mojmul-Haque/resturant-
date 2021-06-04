1c5beea0f16dfd5271b7835e67c1659db33c1f6a
Mojmul-Haque <mojmul150@gmail.com>
2021-06-04 16:19:50 +0600

    folder-created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../sharePage/Nav/Nav";
+
+const Home = () => {
+  return (
+    <main>
+      <Nav />
+    </main>
+  );
+};
+
+export default Home;
